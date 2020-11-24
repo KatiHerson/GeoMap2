24697b9409b8b65face195955a56f0ff382566d9
Test <Test>
2020-11-24 17:04:28 +0300

    Initial Commit

---
+++ GeoMap2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeoMap2
+//
+//  Created by user on 24/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
